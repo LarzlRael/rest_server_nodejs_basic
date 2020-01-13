fb907ac957377902c6566731dc7a760c41e73296
larz <rey_gato10@hotmail.com>
2020-01-12 23:09:13 -0400

    Variable de entorno agregada

--- server/config/config.js
+++ server/config/config.js
@@ -8,11 +8,12 @@ process.env.PORT = process.env.PORT || 3000
 // ============================
 let urlDB;
 
-// if(process.env.NODE_ENV === 'dev'){
-//     urlDB = 'mongodb://localhost/udemy_rest_node';
-// }else{
-urlDB = 'mongodb+srv://larz:fantasticbaby@cluster0-t32kh.mongodb.net/cafe-udemy?retryWrites=true&w=majority';
-//}
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost/udemy_rest_node';
+} else {
+
+    urlDB = process.env.MONGO_URI;
+}
 
 process.env.URLDB = urlDB;
-console.log('valor de la cadena de conexion ' + process.env.URLDB)
+
